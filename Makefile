089c72e38204442462a4c2f8d1320cf4af1f1043
Andrea Zoli <zoli@iasfbo.inaf.it>
2015-05-27 16:50:01 +0200

    Add pwave_omp.

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-all: pthreads mt pwave_serial pwave_cl
+all: pthreads mt pwave_serial pwave_omp pwave_cl
 
 SYSTEM= $(shell gcc -dumpmachine)
 ifneq (, $(findstring linux, $(SYSTEM)))
@@ -13,10 +13,13 @@ pthreads: pthreads.c
 	$(CC) $(CFLAGS) pthreads.c -o pthreads -pthread $(PTHREAD_LIBS)
 
 pwave_serial: pwave_serial.cpp
-	$(CXX) -O3 -g -std=c++11 -I. $(CXXFLAGS) pwave_serial.cpp -o pwave_serial -lpacket -lcfitsio -lCTAConfig -lCTAUtils
+	$(CXX) -O3 -g -std=c++11 $(CXXFLAGS) pwave_serial.cpp -o pwave_serial -lpacket -lcfitsio -lCTAConfig -lCTAUtils
+
+pwave_omp: pwave_serial.cpp
+	$(CXX) -O3 -g -std=c++11 -fopenmp -DOMP $(CXXFLAGS) pwave_serial.cpp -o pwave_omp -lpacket -lcfitsio -lCTAConfig -lCTAUtils
 
 pwave_cl: pwave_cl.cpp
-	$(CXX) -g -std=c++11 -I. $(CXXFLAGS) pwave_cl.cpp -o pwave_cl -lpacket -lcfitsio -lCTAConfig -lCTAUtils -pthread $(OCL_LIBS)
+	$(CXX) -O3 -g -std=c++11 -I. $(CXXFLAGS) pwave_cl.cpp -o pwave_cl -lpacket -lcfitsio -lCTAConfig -lCTAUtils -pthread $(OCL_LIBS)
 
 mt: mt.cpp
 	$(CXX) $(CXXFLAGS) mt.cpp -o mt -lz -lpacket -pthread -lCTAUtils $(PTHREAD_LIBS)
@@ -25,4 +28,4 @@ pwave_cl_altera: pwave_cl.cpp
 	$(CXX) -DCL_ALTERA -g -std=c++11 -I. $(CXXFLAGS) `aocl compile-config` pwave_cl.cpp -o pwave_cl_altera -lpacket -lcfitsio -lCTAConfig -lCTAUtils -lpthread $(OCL_LIBS) `aocl link-config`
 
 clean:
-	@rm -rf pthreads mt pwave_cl pwave_serial pwave_cl.dSYM
+	@rm -rf pthreads mt pwave_cl pwave_serial pwave_omp pwave_cl.dSYM
--- pwave_serial.cpp
+++ pwave_serial.cpp
@@ -7,6 +7,9 @@
 #include <chrono>
 #include <ctime>
 #include <cstdlib>
+#ifdef OMP
+#include <omp.h>
+#endif
 
 //#define DEBUG 1
 
@@ -18,6 +21,8 @@ void waveExtract(unsigned short* inBuf, unsigned short* maxBuf,
                  unsigned short* timeBuf, unsigned int nPixels,
                  unsigned int nSamples, unsigned int windowSize) {
 
+//  alternative to main pragma/critical sections
+//    #pragma omp parallel for default(none) firstprivate(inBuf, maxBuf, timeBuf, nPixels, nSamples, windowSize)
     for(unsigned int pixelIdx=0; pixelIdx<nPixels; pixelIdx++) {
         unsigned int pixelOff = pixelIdx * nSamples;
 
@@ -49,81 +54,112 @@ void waveExtract(unsigned short* inBuf, unsigned short* maxBuf,
 }
 
 int main(int argc, char *argv[]) {
-	if(argc < 4) {
-		std::cout << "Wrong arguments, please provide config file, raw input file and number of events." << std::endl;
-		std::cout << argv[0] << " config.xml input.raw <nevents>" << std::endl;
-		return 1;
-	}
-	const std::string configFilename(realpath(argv[1], NULL));
-	const std::string inputFilename(realpath(argv[2], NULL));
-	const unsigned long numEvents = std::atol(argv[3]);
+    if(argc < 4) {
+        std::cout << "Wrong arguments, please provide config file, raw input file and number of events." << std::endl;
+        std::cout << argv[0] << " config.xml input.raw <nevents>" << std::endl;
+        return 1;
+    }
+    const std::string configFilename(realpath(argv[1], NULL));
+    const std::string inputFilename(realpath(argv[2], NULL));
+    const unsigned long numEvents = std::atol(argv[3]);
 
-	PacketLib::PacketBufferV events(configFilename, inputFilename);
-	events.load();
-	PacketLib::PacketStream ps(configFilename.c_str());
+    PacketLib::PacketBufferV events(configFilename, inputFilename);
+    events.load();
+    PacketLib::PacketStream ps(configFilename.c_str());
 
-	CTAConfig::CTAMDArray array_conf;
-	std::cout << "Preloading.." << std::endl;
-	array_conf.loadConfig("AARPROD2", "PROD2_telconfig.fits.gz", "Aar.conf", "conf/");
-	std::cout << "Load complete!" << std::endl;
+    CTAConfig::CTAMDArray array_conf;
+    std::cout << "Preloading.." << std::endl;
+    array_conf.loadConfig("AARPROD2", "PROD2_telconfig.fits.gz", "Aar.conf", "conf/");
+    std::cout << "Load complete!" << std::endl;
 
     const unsigned int MAX_NPIXELS = 3000;
     unsigned short maxBuf[MAX_NPIXELS];
     unsigned short timeBuf[MAX_NPIXELS];
 
-	time_point<system_clock> start = system_clock::now();
+    time_point<system_clock> start = system_clock::now();
+    time_point<system_clock> startPacket, endPacket;
+    time_point<system_clock> startExtract, endExtract;
+    duration<double> elapsedPacket(0.0), elapsedExtract(0.0);
 
-    unsigned long eventCounter = 0;
     unsigned long byteCounter = 0;
-    while(eventCounter < numEvents) {
+    #pragma omp parallel for default(none) firstprivate(maxBuf, timeBuf, startPacket, endPacket, startExtract, endExtract) shared(elapsedPacket, elapsedExtract, byteCounter, ps, array_conf, events)
+    for(unsigned long eventCounter=0; eventCounter<numEvents; eventCounter++) { 
 
         // Get event data
-        PacketLib::ByteStreamPtr event = events.getNext();
-#ifdef ARCH_BIGENDIAN
-        if(!event->isBigendian())
-            event->swapWord();
-#else
-        if(event->isBigendian())
-            event->swapWord();
-#endif
-        PacketLib::Packet *packet = ps.getPacket(event);
-        PacketLib::byte* buff = packet->getData()->getStream();
-        PacketLib::dword buffSize = packet->getData()->size();
-        PacketLib::DataFieldHeader* dfh = packet->getPacketDataFieldHeader();
-        const unsigned int telescopeID = dfh->getFieldValue_16ui("TelescopeID");
-        CTAConfig::CTAMDTelescopeType* teltype = array_conf.getTelescope(telescopeID)->getTelescopeType();
-        int telTypeSim = teltype->getID();
-        const unsigned int npixels = teltype->getCameraType()->getNpixels();
-        const unsigned int nsamples = teltype->getCameraType()->getPixel(0)->getPixelType()->getNSamples();
+        PacketLib::byte* buff;
+        PacketLib::dword buffSize;
+        unsigned int npixels;
+        unsigned int nsamples;
         const unsigned int windowSize = 6;
+        #pragma omp critical
+        {
+            startPacket = system_clock::now();
+
+            PacketLib::ByteStreamPtr event = events.getNext();
+
+#ifdef ARCH_BIGENDIAN
+            if(!event->isBigendian())
+                event->swapWord();
+#else
+            if(event->isBigendian())
+                event->swapWord();
+#endif
+            PacketLib::Packet *packet = ps.getPacket(event);
+            buff = packet->getData()->getStream();
+            buffSize = packet->getData()->size();
+            PacketLib::DataFieldHeader* dfh = packet->getPacketDataFieldHeader();
+            const unsigned int telescopeID = dfh->getFieldValue_16ui("TelescopeID");
+            CTAConfig::CTAMDTelescopeType* teltype = array_conf.getTelescope(telescopeID)->getTelescopeType();
+            int telTypeSim = teltype->getID();
+            npixels = teltype->getCameraType()->getNpixels();
+            nsamples = teltype->getCameraType()->getPixel(0)->getPixelType()->getNSamples();
+
+            endPacket = system_clock::now();
+            elapsedPacket += endPacket - startPacket;
+            startExtract = system_clock::now();
+        }
 
         // Compute waveform extraction
         waveExtract((unsigned short*)buff, maxBuf, timeBuf, npixels, nsamples, windowSize);
 
+
+        #pragma omp critical
+        {
+            endExtract = system_clock::now();
+            elapsedExtract += endExtract - startExtract;
+        }
+
 #ifdef DEBUG
-		std::cout << "npixels: " << npixels << std::endl;
-		std::cout << "nsamples: " << nsamples << std::endl;
-		for(int pixelIdx = 0; pixelIdx<npixels; pixelIdx++) {
-			PacketLib::word* s = (PacketLib::word*) buff + pixelIdx * nsamples;
-			std::cout << "pixel: " << pixelIdx << " samples: ";
-			for(int sampleIdx=0; sampleIdx<nsamples; sampleIdx++) {
-				std::cout << s[sampleIdx] << " ";
-			}
-			std::cout << std::endl;
-			std::cout << "result: " << " " << maxBuf[pixelIdx] << " " << timeBuf[pixelIdx] << " " << std::endl;
-		}
+        std::cout << "npixels: " << npixels << std::endl;
+        std::cout << "nsamples: " << nsamples << std::endl;
+        for(int pixelIdx = 0; pixelIdx<npixels; pixelIdx++) {
+            PacketLib::word* s = (PacketLib::word*) buff + pixelIdx * nsamples;
+            std::cout << "pixel: " << pixelIdx << " samples: ";
+            for(int sampleIdx=0; sampleIdx<nsamples; sampleIdx++) {
+                std::cout << s[sampleIdx] << " ";
+            }
+            std::cout << std::endl;
+            std::cout << "result: " << " " << maxBuf[pixelIdx] << " " << timeBuf[pixelIdx] << " " << std::endl;
+        }
 #endif
-        eventCounter++;
+        #pragma omp atomic
         byteCounter += buffSize;
     }
 
-	time_point<system_clock> end = system_clock::now();
+    time_point<system_clock> end = system_clock::now();
 
-	duration<double> elapsed = end-start;
-	double throughputE = eventCounter / elapsed.count();
-	double throughputB = byteCounter / elapsed.count() / 1000000;
-	std::cout << eventCounter << " events sent in " << elapsed.count() << " s" << std::endl;
-	std::cout << "throughput: " << throughputE << " events/s - " << throughputB << " MB/s" << std::endl;
+    duration<double> elapsed = end-start;
+    double throughputE = numEvents / elapsed.count();
+    double throughputB = byteCounter / elapsed.count() / 1000000;
+    std::cout << numEvents << " events" << std::endl;
+    std::cout << "Elapsed packet " << elapsedPacket.count() << std::endl;
+#ifndef OMP
+    std::cout << "Elapsed extract " << elapsedExtract.count() << std::endl;
+#else
+    std::cout << "Elapsed extract " << elapsedExtract.count() / omp_get_max_threads() << std::endl;
+#endif
+    std::cout << "Elapsed total " << elapsed.count() << std::endl;
+    std::cout << "throughput: " << throughputE << " events/s - " << throughputB << " MB/s" << std::endl;
 
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
